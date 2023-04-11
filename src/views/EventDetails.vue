28dd19bb9a9848c3a983b0ee9a1b691544be90e7
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-04-11 15:12:11 +0200

    Adding fallback error page (404) to event sub-url

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -4,6 +4,9 @@
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
   </div>
+  <div v-else>
+    <h1>The page you're looking for was not found. (404)</h1>
+  </div>
 </template>
 
 <script>
