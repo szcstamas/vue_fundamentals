b2b331c76069d5d9603dfb39b6f9f8d4b9072a43
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-04-12 09:43:44 +0200

    Adding sass-loader dependency

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,25 +1,64 @@
 <template>
-  <div>Create an Event, {{ user.id }}</div>
-  <p>This event was created by {{ user.name }}</p>
-  <p>There are {{ catLength }} categories</p>
-  <p>{{ getEventById(2) }}</p>
-  <ul>
-    <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-  </ul>
+  <div>Create an Event</div>
+  <form>
+    <label>Select a category</label>
+    <select v-model="event.category">
+      <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+    </select>
+    <h3>Name & describe your event</h3>
+    <div class="field">
+      <label>Title</label>
+      <input
+        v-model="event.title"
+        type="text"
+        placeholder="Add an event title"
+      />
+    </div>
+    <div class="field">
+      <label>Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Add a description"
+      />
+    </div>
+    <h3>Where is your event?</h3>
+    <div class="field">
+      <label>Location</label>
+      <input
+        v-model="event.location"
+        type="text"
+        placeholder="Add a location"
+      />
+    </div>
+    <h3>When is your event?</h3>
+    <div class="field">
+      <label>Date</label>
+      <datepicker v-model="event.date" placeholder="Select a date" />
+    </div>
+    <div class="field">
+      <label>Select a time</label>
+      <select v-model="event.time">
+        <option v-for="time in times" :key="time">{{ time }}</option>
+      </select>
+    </div>
+    <input type="submit" class="button -fill-gradient" value="Submit" />
+  </form>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 export default {
-  computed: {
-    catLength() {
-      return this.$store.getters.catLength;
-    },
-    ...mapGetters(["getEventById"]),
-    ...mapState(["user", "categories"]),
+  components: {
+    VueDatePicker,
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.field {
+  margin-bottom: 20px;
+}
+</style>
